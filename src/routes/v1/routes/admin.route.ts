7868b411d34cc8371fea5213cc6646c5c6391623
Alireza Aberoumand <maya7192@yahoo.com>
2025-12-19 18:26:50 -0800

    Refactor admin routes: integrate access control middleware for ban and unban user functionality, enhancing security and clarity in route definitions.

--- src/routes/v1/routes/admin.route.ts
+++ src/routes/v1/routes/admin.route.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import { banUser, unBanUser } from 'controllers/v1/admin.controller.ts';
+import accessByLevelMiddleware from 'middlewares/accessByLevel.middleware.ts';
 
 const router = Router();
 
-router.post('/ban-user/:violatorId', banUser);
-router.post('/unban-user/:violatorId', unBanUser);
+router.post(
+  '/ban-user/:violatorId',
+  accessByLevelMiddleware([3], 'Only admins is allowed to ban a user'),
+  banUser
+);
+router.post(
+  '/unban-user/:violatorId',
+  accessByLevelMiddleware([3], 'Only admins is allowed to unban a user'),
+  unBanUser
+);
 
 export default router;
--- src/routes/v1/v1.route.ts
+++ src/routes/v1/v1.route.ts
@@ -3,17 +3,10 @@ import authRoutes from './routes/auth.route.ts';
 import adminRoutes from './routes/admin.route.ts';
 import authMiddleware from 'middlewares/auth.middleware.ts';
 import roleLevelMiddleware from 'middlewares/roleLevel.middleware.ts';
-import accessByLevelMiddleware from 'middlewares/accessByLevel.middleware.ts';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use(
-  '/admin',
-  authMiddleware,
-  roleLevelMiddleware,
-  accessByLevelMiddleware([3], 'Only admins is allowed to ban a user'),
-  adminRoutes
-);
+router.use('/admin', authMiddleware, roleLevelMiddleware, adminRoutes);
 
 export default router;
